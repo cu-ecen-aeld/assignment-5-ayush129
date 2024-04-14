87bbc1cb464e4c984fa9e07df63ee1bbcd8e181b
Ayush Mittal <mittalayush129@gmail.com>
2024-04-14 19:16:47 +0530

    Assignemnt-4-Part-2 working properly on local machine

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4675e5cd5be58c7448b0a31f20c8d65e53de094b
+AESD_ASSIGNMENTS_VERSION = 680d19961429dac4de8a173066bcd4291e1c977d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script to Reset Buildroot for a new target: To delete all build products as well as the configuration:
+#Author: Ayush Mittal
+
+cd ./buildroot
+make distclean
